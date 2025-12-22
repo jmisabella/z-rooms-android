3f277001fbc37b0faa73023a887d442724fc9b56
Jeffrey Isabella <78503766+jmisabella@users.noreply.github.com>
2025-12-21 21:26:51 -0500

    version 1.5: adjusted presets; morning greeting; fix preset limit bug
    bug was that random presets were limited to only first 10 presets instead of including all 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.jmisabella.zrooms"
         minSdk = 21
         targetSdk = 36
-        versionCode = 20
-        versionName = "1.4"
+        versionCode = 21
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
