d25d264ecf7371e877cc14dce912ca0356672ab4
Jeffrey Isabella <78503766+jmisabella@users.noreply.github.com>
2025-12-30 22:40:25 -0500

    version 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.jmisabella.zrooms"
         minSdk = 21
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.5"
+        versionCode = 22
+        versionName = "1.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
